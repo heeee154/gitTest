40907f61e4290904c879e88fdf57cb3c8debc7ee
LJY <@.>
2020-02-21 15:42:11 +0900

    2nd edit

--- src/gitTest/GitTest.java
+++ src/gitTest/GitTest.java
@@ -2,7 +2,7 @@ package gitTest;
 
 public class GitTest {
 	public static void main(String[] arg) {
-		System.out.println("hello gitTest hello world");
+		System.out.println("2nd edit 수정합니다");
 	}
 
 }
